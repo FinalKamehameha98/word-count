b5dfb1f2f03ef9a1276c2a50013ca66715ad36d0
Andres Rivera <andres.rivera@twc.com>
2022-07-20 00:21:36 -0700

    Initialize testing script and add comments to program

---
+++ simple_wc.c
@@ -0,0 +1,17 @@
+/**
+ * Simple Word Count Utility
+ * Author: Andres Rivera
+ * 
+ * It prints out the number of characters, words, and lines in a given file(s)
+ * or from standard input.  It will also print out a total for each count if
+ * it is given more than 1 file.
+ */
+
+#include <stdio.h>
+#include <ctype.h>
+
+
+int main(int argc, char *argv[]){
+    
+}
+
---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Testing with No Files (AKA Using stdin)
+
+echo "Testing with No Files"
+echo "_____________________"
+make clean 
+
+# Testing with 1 File
+
+echo "Testing with 1 File"
+
+
+# Testing with >1 Files
+
+echo "Testing with >1 Files"
+echo "---------------------"
